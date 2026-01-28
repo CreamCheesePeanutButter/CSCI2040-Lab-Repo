d35caf2d82d8d76f255aae0989da06c05a45e5a1
CreamCheesePeanutButter <lonelyaraya@gmail.com>
2026-01-28 08:56:56 -0500

    add front

--- draft/src/App.jsx
+++ draft/src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/data")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
   return (
     <>
       <h1>Hello, World!</h1>
-      <ol></ol>
+      <ol>
+        {data.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ol>
     </>
   );
 }
